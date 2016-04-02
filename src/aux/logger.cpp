df7c9478d34fe32f499afddc31998c9cbcbf1133
K17K47 <k17k47@gmail.com>
2016-04-01 23:20:08 -0300

    Impl inicial completa de logger

---
+++ src/aux/logger.cpp
@@ -0,0 +1,14 @@
+#include<stddef.h>
+#include<iostream>
+
+#include"aux/logger.hpp"
+
+static Logger& Logger::instance(){
+   static Logger _instance();
+   return _instance;
+}
+
+void Logger::printMsg(std::string funcID,std::string msg){
+   std::chrono::duration<double> t=std::chrono::steady_clock::now()-start;
+   std::cout<<"["<<t.count()<<"] "<<funcID<<": "<<msg<<"\n";
+}
