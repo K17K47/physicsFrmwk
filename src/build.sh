5c83254cf4107f6bb8ec43b4cb35bd1f71dfeb3c
K17K47 <k17k47@gmail.com>
2016-04-29 20:38:33 -0300

    Updated build script for new files

--- src/build.sh
+++ src/build.sh
@@ -3,4 +3,6 @@
 g++ -c particles/particle.cpp -o build/particles/particle.o -I./ -fPIC -std=c++11
 g++ -c particles/pMngr.cpp -o build/particles/pMngr.o -I./ -fPIC -std=c++11
 g++ -c particles/pWorld.cpp -o build/particles/pWorld.o -I./ -fPIC -std=c++11
-g++ -shared -o build/libPhysics.so build/particles/particle.o build/particles/pMngr.o build/particles/pWorld.o
+g++ -c particles/pForceGens.cpp -o build/particles/pForceGens.o -I./ -fPIC -std=c++11
+g++ -c particles/forceGenerators.cpp -o build/particles/forceGenerators.o -I./ -fPIC -std=c++11
+g++ -shared -o build/libPhysics.so build/particles/particle.o build/particles/pMngr.o build/particles/pWorld.o build/particles/pForceGens.o build/particles/forceGenerators.o
