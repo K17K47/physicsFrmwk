3d2aaf22a3f1002e2c57e38a7dcfd6f2acd984e2
K17K47 <k17k47@gmail.com>
2017-11-25 21:38:04 -0200

    Internal slotMap data structures are now private

--- src/aux/slotmap.hpp
+++ src/aux/slotmap.hpp
@@ -5,22 +5,24 @@
 
 template<class T>
 class slotMap{
-   std::vector<T> data;
-   std::vector<unsigned> bRef;
-   std::vector<unsigned> indir;
-   std::vector<unsigned> free;
+   private:
+      std::vector<T> data;
+      std::vector<unsigned> bRef;
+      std::vector<unsigned> indir;
+      std::vector<unsigned> free;
 
-   // Insertion and erasing of elements
-   unsigned insert(T item);   // Returns object ID
-   void erase(unsigned idx);  // Uses object ID
+   public:
+      // Insertion and erasing of elements
+      unsigned insert(T item);   // Returns object ID
+      void erase(unsigned idx);  // Uses object ID
 
-   // Overload for access using object ID
-   T& operator[](const unsigned idx);
-   const T& operator[](const unsigned idx);
+      // Overload for access using object ID
+      T& operator[](const unsigned idx);
+      const T& operator[](const unsigned idx);
 
-   // Direct Data access
-   T& get(const unsigned idx);
-   const T& get(const unsigned idx);
+      // Direct Data access
+      T& get(const unsigned idx);
+      const T& get(const unsigned idx);
 };
 
 #endif
